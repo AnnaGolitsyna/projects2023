5cb28bc724c50010aec8ff74b9dc1479689f9d26
AnnaGolitsyna <110576gav@gmail.com>
2023-08-24 18:09:17 +0300

    iconStyle

--- coffee/src/components/messageModal/MessageModal.jsx
+++ coffee/src/components/messageModal/MessageModal.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 import './messageModal.css';
+import pirateImage from '../../styles/background/pirate.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faTelegram,
+  faFacebook,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 const modalEl = document.getElementById('modal-root');
 
 const MessageModal = () => {
   return createPortal(
     <div className="modal__container">
-      <div className='modal__content'> MessageModal</div>
+      <div className="modal__content">
+        <div className="modal__communication">
+          <form className="dilog">MessageModal</form>
+          <div className="iconsGroup">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="icon icon_instagram"
+            />
+            <FontAwesomeIcon icon={faTelegram} className="icon icon_telegram" />
+            <FontAwesomeIcon icon={faFacebook} className="icon icon_facebook" />
+            <FontAwesomeIcon icon={faTwitter} className="icon icon_twitter" />
+          </div>
+        </div>
+        <img className="modal__img" src={pirateImage} alt="pirate" />
+        <button className="btn__close"> X </button>
+      </div>
     </div>,
     modalEl
   );
