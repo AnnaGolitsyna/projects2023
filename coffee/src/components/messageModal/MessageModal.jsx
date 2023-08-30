3df426f39a8fb0d36ec8bb596be19193bdcb4abf
AnnaGolitsyna <110576gav@gmail.com>
2023-08-30 16:28:35 +0300

    loading

--- coffee/src/components/messageModal/MessageModal.jsx
+++ coffee/src/components/messageModal/MessageModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createPortal } from 'react-dom';
+import  { createPortal } from 'react-dom';
 import { useForm } from 'react-hook-form';
 import SocialMediaBar from '../socialMediaBar/SocialMediaBar';
 import DialogForm from '../dialogForm/DialogForm';
@@ -8,6 +8,12 @@ import './messageModal.css';
 
 const modalEl = document.getElementById('modal-root');
 
+const preloadedImage = new Image();
+preloadedImage.src = pirateImage;
+preloadedImage.onload = () => {
+  renderModal();
+};
+
 const MessageModal = ({ closeModal }) => {
   const {
     register,
@@ -15,7 +21,7 @@ const MessageModal = ({ closeModal }) => {
     formState: { errors },
   } = useForm();
 
-  return createPortal(
+  return (
     <div className="modal__container">
       <div className="modal__content">
         <div className="modal__communication">
@@ -37,9 +43,38 @@ const MessageModal = ({ closeModal }) => {
           X{' '}
         </button>
       </div>
-    </div>,
-    modalEl
+    </div>
   );
 };
 
+const renderModal = () => {
+ return createPortal(<MessageModal />, modalEl);
+};
+
 export default MessageModal;
+
+// return createPortal(
+//   <div className="modal__container">
+//     <div className="modal__content">
+//       <div className="modal__communication">
+//         <p className="modal__title">
+//           Our manager will be happy to read your message
+//         </p>
+//         <DialogForm
+//           register={register}
+//           errors={errors}
+//           handleSubmit={handleSubmit}
+//           closeModal={closeModal}
+//         />
+
+//         <SocialMediaBar />
+//       </div>
+//       <img className="modal__img" src={pirateImage} alt="pirate" />
+//       <button className="btn__close-message" onClick={closeModal}>
+//         {' '}
+//         X{' '}
+//       </button>
+//     </div>
+//   </div>,
+//   modalEl
+// );
